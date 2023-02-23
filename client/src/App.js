fef5362bbf359e01ff20519946c339060e70549a
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2023-02-23 09:18:16 +0600

    socket intregate on react app client and working well

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,50 @@
-
+import { io } from "socket.io-client"
+import { useEffect, useState } from "react"
 
 function App() {
+
+  const [message, setMessage] = useState("");
+  const [serverMessage, setServerMessage] = useState("");
+
+  const socket = io.connect("http://localhost:5000");
+  console.log(socket);
+  const handleSubmit = e => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    console.log(name, email);
+    setMessage(name)
+    socket.emit("reactEvent", { name, email })
+  }
+
+  useEffect(() => {
+    socket.on("servermessage", (data) => {
+      console.log("data useEffect under:", data);
+      if (data) {
+        setServerMessage(data?.name)
+      }
+    })
+  }, [message, serverMessage, socket])
+
   return (
     <div >
-      <h1>This is app.js</h1>
+      <div>
+        <h1>This is app.js</h1>
+      </div>
+      <div>
+        <p>send: {message}</p>
+        <p>received:{serverMessage}</p>
+      </div>
+      <div>
+        <form onSubmit={e => handleSubmit(e)} >
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" required />
+          <label htmlFor="email" >Email</label>
+          <input type="email" name="email" required />
+
+          <input type="submit" value="send" />
+        </form>
+      </div>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+const cors = require('cors');
 require("dotenv").config();
 require("colors")
-const path = require("path")
 const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware--------------
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
 
 const http = require("http");
 const httpServer = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+});
 
 
 
@@ -18,18 +27,38 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", (socket) => {
         console.log("event disconnected");
-    });
+    })
 
-    socket.send("this is from serverside.");
+    //     socket.send("this is from serverside.");
 
-    // socket.on("message", (data) => {
-    //     console.log(data);
-    // })
-    // socket.on("testEvent", (data) => {
-    //     console.log(data);
-    // })
-    io.sockets.emit("testingFebruary", "Oma go Ture Love")
+    //     // socket.on("message", (data) => {
+    //     //     console.log(data);
+    //     // })
+    //     // socket.on("testEvent", (data) => {
+    //     //     console.log(data);
+    //     // })
+    //     io.sockets.emit("testingFebruary", "Oma go Ture Love");
+
+    socket.on("reactEvent", (data) => {
+        console.log(data);
+        socket.emit("servermessage", data)
+    })
 })
+
+// let fifa = io.of("/worldCup");
+// fifa.on("connection", (socket) => {
+//     fifa.emit("febEvent", "Hello this is testing on februay month")
+// })
+
+// let cricket = io.of("/cricket");
+// cricket.on("connection", (socket) => {
+//     cricket.emit("cricketEvent", "This is from mirpur cricket.")
+// })
+
+// let hocky = io.of("/hockyworld");
+// hocky.on("connection", (socket) => {
+//     hocky.emit("hockyEvent", "This is from hockye event data.")
+// })
 
 
 app.get("/html", (req, res) => {
